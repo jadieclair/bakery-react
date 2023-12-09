2e69c3a4b7a6c8ddf1a40e27368338277457a8f6
jadeharris <jadieclair@yahoo.com>
2023-12-09 16:28:59 +0200

    error fixed

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,13 +7,12 @@ export default function Cards(props) {
       <div className="card-body">
         <h5 className="card-title">{props.title}</h5>
         <p className="card-text">{props.detail}</p>
-        <a
-          href="#"
+        <button
           className="btn btn-primary"
           style={{ color: "black", backgroundColor: "grey", border: "none" }}
         >
           Place Order
-        </a>
+        </button>
       </div>
     </div>
   );
